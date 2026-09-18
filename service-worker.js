e925007d255a2f685450d216370104253804dc15
salvadorayn-sudo <salvadorayn@gmail.com>
2026-09-18 17:57:26 -0400

    Add service worker for caching files, Commit directly to the main branch is selected

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "pixiepurse-v2";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./pixiepurse-fairy-logo.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
